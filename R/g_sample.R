f91f7f07c3feded9b8a78d1f6f168732b680a27d
Lief <williamlief@gmail.com>
2019-06-05 15:47:28 -0700

    g_sample now covers sample_frac and sample_n

---
+++ R/g_sample.R
@@ -0,0 +1,53 @@
+
+#' Sample groups from a table
+#'
+#'@name g_sample
+#' @description using group_by with sample returns a sample of the observations
+#' within a group. This function instead returns all observations for a sample
+#' of groups.
+#'
+#' @param tbl tbl of data
+#' @param group grouping variable
+#' @inheritParams dplyr::sample
+#' @seealso \code{\link[dplyr]{sample}}
+#' @examples
+#' g_sample_frac(airquality, Month, .3)
+#' g_sample_n(airquality, Month, 3)
+NULL
+
+## NULL
+
+#' @rdname g_sample
+#' @export
+g_sample_frac <- function(tbl, group, ...) {
+
+    group <- rlang::enquo(group)
+    liefTools::check_vars_exist(tbl, !!group)
+
+
+    sample <-
+      dplyr::select(tbl, !!group) %>%
+      dplyr::distinct() %>%
+      dplyr::sample_frac(...) %>%
+      dplyr::left_join(tbl)
+
+    return(sample)
+}
+
+
+
+#' @rdname g_sample
+#' @export
+g_sample_n <- function(tbl, group, ...) {
+
+  group <- rlang::enquo(group)
+  liefTools::check_vars_exist(tbl, !!group)
+
+  sample <-
+    dplyr::select(tbl, !!group) %>%
+    dplyr::distinct() %>%
+    dplyr::sample_n(...) %>%
+    dplyr::left_join(tbl)
+
+  return(sample)
+}
